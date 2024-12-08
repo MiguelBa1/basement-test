6bcdd62c8314eb66a4122290741274ba34ff1820
miguel <miguelbarreraa05@gmail.com>
2024-12-08 18:56:24 -0500

    feat: update typescript version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import {useRouter} from "next/router";
 import {Provider} from "react-redux";
 
 import {store} from "../store";
-
 import "../css/global.css";
 import MetaTags from "../components/MetaTags";
 
